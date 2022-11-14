cb186d2770ad55530d67c153eb0006165f2445b6
santosh <santhosh.t@nuacem.com>
2022-11-14 18:46:24 +0530

    Angular Firebase APP

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB_11_zdA8QbVAEaSyRsrJLxQw280Fd-QA',
+    authDomain: 'kanbanfire-58288.firebaseapp.com',
+    projectId: 'kanbanfire-58288',
+    storageBucket: 'kanbanfire-58288.appspot.com',
+    messagingSenderId: '781686908853',
+    appId: '1:781686908853:web:81da5291640fc1f4b11bfc',
+    measurementId: 'G-HH46SXQ7RJ',
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB_11_zdA8QbVAEaSyRsrJLxQw280Fd-QA',
+    authDomain: 'kanbanfire-58288.firebaseapp.com',
+    projectId: 'kanbanfire-58288',
+    storageBucket: 'kanbanfire-58288.appspot.com',
+    messagingSenderId: '781686908853',
+    appId: '1:781686908853:web:81da5291640fc1f4b11bfc',
+    measurementId: 'G-HH46SXQ7RJ',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
